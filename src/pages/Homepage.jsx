681669fb827a6d8934fc3ea9ef87083054f980e3
Aya Debbagh <adebb027@uottawa.ca>
2024-06-30 22:02:35 -0400

    footer

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,8 @@
 import pinkFooter from "../assets/images/footerPink.png";
+import facebook from "../assets/images/facebook.png";
+import instagram from "../assets/images/instagram.png";
+import tiktok from "../assets/images/tiktok.png";
+import twitter from "../assets/images/twitter.png";
 import Navbar from "../components/navBar.jsx";
 
 function Homepage() {
@@ -19,21 +23,42 @@ function Homepage() {
           style={{ top: "5px" }}
         />
         <div className="flex justify-center items-center w-full absolute top-0 left-0 right-0 bottom-0">
-          <div className="flex justify-center items-center w-full px-8">
-            <div
-              className="text-left mr-8 md:mr-20 lg:mr-20 text-red font-Texterius text-md md:text-xl lg:text-2xl whitespace-nowrap"
-              style={{ minWidth: "250px" }}
-            >
-              Contact us: <br />
-              TheSparkCo@gmail.com <br />
-              (613) 123-4567
+          <div className="flex flex-col justify-center items-center h-full w-full px-8">
+            <div className="flex justify-center items-center flex-wrap">
+              <div
+                className="text-left mr-8 md:mr-20 lg:mr-20 text-red font-Texterius text-md md:text-xl lg:text-2xl whitespace-nowrap mb-4 md:mb-4 pl-9 pr-9"
+                style={{
+                  minWidth: "250px",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                }}
+              >
+                Contact us: <br />
+                TheSparkCo@gmail.com <br />
+                (613) 123-4567
+              </div>
+              <div
+                className="text-left md:ml-20 lg:ml-20 text-red font-Texterius text-md md:text-xl lg:text-2xl whitespace-nowrap mb-4 md:mb-4 pl-9"
+                style={{
+                  minWidth: "250px",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                }}
+              >
+                Made with love by <br />
+                The Spark Collective team!
+              </div>
             </div>
-            <div
-              className="text-left ml-8 md:ml-20 lg:ml-20 text-red font-Texterius text-sm md:text-xl lg:text-2xl whitespace-nowrap"
-              style={{ minWidth: "250px" }}
-            >
-              Made with love by <br />
-              The Spark Collective team!
+            <div className=" absolute bottom-20 flex justify-center items-center space-x-4">
+              <img src={instagram} className="w-7 h-7" />
+              <img src={facebook} className="w-7 h-7" />
+              <img src={tiktok} className="w-7 h-7" />
+              <img src={twitter} className="w-7 h-7" />
+            </div>
+            <div className="absolute bottom-0 text-center">
+              <div className=" text-red font-Texterius text-md md:text-xl lg:text-2xl mt-4 w-full">
+                TSP
+              </div>
             </div>
           </div>
         </div>
